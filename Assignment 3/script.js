c9f05077282bbdead20688b4e8af1e22daa7d99d
ruchi-kapadiwala-nu <ruchikapadiwala@Ruchis-MacBook-Air.local>
2023-02-02 16:47:35 -0500

    Beautified the code and added comments

--- Assignment 3/script.js
+++ Assignment 3/script.js
@@ -3,17 +3,26 @@ var count = 1;
 var submitSelectedAwards = document.getElementById("button");
 
 function dropDownTextAreaToggle(){
+  //getting all the images in a list "imageTagList"
   var imageTagList = document.getElementsByTagName("img");
 
+  //setting the class name for each image as "arrow"
   for (i = 0; i < imageTagList.length; i++) {
     imageTagList[i].className = "arrow";
   }
 
+  //getting all the elements with classname "arrow"
   var arrow = document.getElementsByClassName("arrow");
 
+  
   for (i = 0; i < arrow.length; i++) {
+    //Applying function for all images with class arrow
     arrow[i].onclick = function () {
+
+      //getting rows with classname "dropDownTextArea"
       var dropDownTextArea = this.parentNode.parentNode.nextElementSibling;
+
+      //setting row visibility if "none"
       if (dropDownTextArea.style.display === "none") {
         dropDownTextArea.style.display = "table-row";
       } else {
@@ -25,6 +34,8 @@ function dropDownTextAreaToggle(){
 }
 
 function addNewStudent() {
+
+  //Capturing the latest index of Student and Teacher
   var rows = document.getElementById("myTable").rows;
   var latestIndex;
   if (rows.length > 1) {
@@ -37,6 +48,7 @@ function addNewStudent() {
   var table = document.getElementById("myTable");
   var tbodyRef = document.getElementsByTagName("tbody")[0];
 
+  //creating a new Student details row
   var tdNode = document.createElement("tr");
 
   var trCheckboxCell = document.createElement("td");
@@ -56,7 +68,6 @@ function addNewStudent() {
   var trPercentCell = document.createElement("td");
   trPercentCell.innerHTML = "100%";
 
-
   //appending inside tr
   tdNode.appendChild(trCheckboxCell);
   tdNode.appendChild(trStudentCell);
@@ -70,15 +81,20 @@ function addNewStudent() {
   //appending inside tBody
   tbodyRef.appendChild(tdNode);
 
+  //creating a new Advisor details row
   tdNode = document.createElement("tr");
   tdNode.setAttribute("class", "dropDownTextArea");
+
   var trAdvisorDetails = document.createElement("td");
   trAdvisorDetails.setAttribute("colspan", "8")
   trAdvisorDetails.innerHTML = 'Advisor:<br /><br />Award Details<br />Summer 1-2014(TA)<br />Budget Number: <br />Tuition Number: <br />Comments:<br /><br /><br />Award Status:<br /><br /><br />'
 
+  //appending inside tr
   tdNode.appendChild(trAdvisorDetails);
 
+  //appending inside tBody
   tbodyRef.appendChild(tdNode);
+
   count++;
 
   alert("Record added successfully...!!");
@@ -88,35 +104,37 @@ function addNewStudent() {
 
 function onCheckboxClick(checkbox) {
   var rowSelect = checkbox.parentElement.parentElement;
-  
 
   if (checkbox.checked == true) {
+
     //Changing color of background and button
     rowSelect.style.backgroundColor = "yellow";
     submitSelectedAwards.style.backgroundColor = "orange";
     submitSelectedAwards.style.borderColor = "orange";
 
-
+    //getting all the rows
     var trList = document.getElementsByTagName("tr");
 
+    //checking if the last column of the table is "Edit"
     if (trList[0].lastElementChild.innerHTML !== "Edit") {
 
-    //setting Column Names
-    var table = document.getElementById("myTable");
-    var tHeadRef = document.getElementsByTagName("thead")[0];
-    var tdNode = tHeadRef.getElementsByTagName("tr")[0];
+      //setting Column Names
+      var table = document.getElementById("myTable");
+      var tHeadRef = document.getElementsByTagName("thead")[0];
+      var tdNode = tHeadRef.getElementsByTagName("tr")[0];
 
-    var trDelete = document.createElement("th");
-    trDelete.innerHTML = "Delete";
-    var trEdit = document.createElement("th");
-    trEdit.innerHTML = "Edit";
+      var trDelete = document.createElement("th");
+      trDelete.innerHTML = "Delete";
+      var trEdit = document.createElement("th");
+      trEdit.innerHTML = "Edit";
 
-    //appending inside tr
-    tdNode.appendChild(trDelete);
-    tdNode.appendChild(trEdit);
+      //appending inside tr
+      tdNode.appendChild(trDelete);
+      tdNode.appendChild(trEdit);
 
-    //appending inside tHead
-    tHeadRef.appendChild(tdNode);
+      //appending inside tHead
+      tHeadRef.appendChild(tdNode);
+
     }
 
     //delete button
@@ -145,15 +163,21 @@ function onCheckboxClick(checkbox) {
   }
 }
 
+//Ensuring the submit button is orange when checkbox is checked
 function submitFunction() {
 
+  //Getting all the input tags
   var inputList = document.getElementsByTagName("input");
 
+  //Setting their classname to "checkbox"
   for (i = 0; i < inputList.length; i++) {
     inputList[i].className = "checkbox";
   }
 
+  //Getting all the elements whose class is "checkbox"
   var checkbox = document.getElementsByClassName("checkbox");
+
+  //Setting the enableSubmitButton flag 
   var enableSubmitButton = false;
 
   for (j = 0; j < checkbox.length; j++) {
@@ -163,10 +187,11 @@ function submitFunction() {
     }
   }
 
+  //Getting all the button with id = "button"
   var buttonElement = document.getElementById("button");
 
   if (!enableSubmitButton) {
-    removeOrNotDeleteEditHeader();
+    resetColumns();
   }
   else {
     buttonElement.style.backgroundColor = "orange";
@@ -174,13 +199,16 @@ function submitFunction() {
   }
 }
 
-function removeOrNotDeleteEditHeader() {
+//Checks whether or not to remove the Delete and Edit Columns
+function resetColumns() {
+
   var trList = document.getElementsByTagName("tr");
 
   trList[0].removeChild(trList[0].lastElementChild);
   trList[0].removeChild(trList[0].lastElementChild);
 }
 
+//Deletes a particular row
 function deleteRow(rowObject) {
   var tr = rowObject.parentElement.parentElement; //tr reference
 
@@ -191,14 +219,16 @@ function deleteRow(rowObject) {
   document.getElementById("myTable").deleteRow(tr.rowIndex);
 
   count--;
-  alert("Record of student deleted successfully");
+  alert("Record of student deleted successfully..!!");
   submitFunction();
 }
 
+//Edits a particular row
 function editRow(rowObject) {
   prompt("Edit the details");
 }
 
+//Responsible for adding the "Delete" and "Edit" buttons
 function onArrowClick(checkbox) {
   var rowSelect = checkbox.parentElement.parentElement;
 
@@ -211,7 +241,7 @@ function onArrowClick(checkbox) {
     deleteButton.innerHTML = '<button id = "delete" type = "button" onclick = "deleteRow(this)">Delete</button>';
     rowSelect.appendChild(deleteButton);
 
-    //delete button
+    //edit button
     var editButton = document.createElement("td");
     editButton.setAttribute("id", "editId");
     editButton.innerHTML = '<button id = "edit" type = "button" onclick = "editRow(this)">Edit</button>';
